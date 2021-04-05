a88f19f36b91a7d0c24ba35dd6c4653285ba4ed6
ptaszor3 <54142803+ptaszor3@users.noreply.github.com>
2021-04-05 15:49:16 +0200

    Delete double_seconds.cpp

--- double_seconds.cpp
+++
@@ -1,9 +0,0 @@
-#include "double_seconds.hpp"
-
-double_seconds operator"" _ds(long double duration) {
-	return double_seconds{duration};
-}
-
-double_seconds operator"" _ds(unsigned long long int duration) {
-	return double_seconds{duration};
-}
