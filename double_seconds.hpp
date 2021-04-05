ef87b2a2b1e65076372a2506ca12c71647695eef
ptaszor3 <54142803+ptaszor3@users.noreply.github.com>
2021-04-05 15:49:08 +0200

    Delete double_seconds.hpp

--- double_seconds.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <chrono>
-
-using double_seconds = std::chrono::duration<double>;
-
-double_seconds operator"" _ds(long double duration);
-double_seconds operator"" _ds(unsigned long long int duration);
